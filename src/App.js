c4b7ff3ca60af457e33b8295eed2a513bbf3e915
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-07-18 17:01:22 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import UploadForm from "./components/UploadForm"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ImageList from "./components/ImageList";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div style={{ maxWidth: 600, marginLeft: "auto", marginRight: "auto"}}>
+    <ToastContainer />
+    <h2>사진첩</h2>
+    <UploadForm />
+    <ImageList />
     </div>
   );
-}
+};
 
 export default App;
